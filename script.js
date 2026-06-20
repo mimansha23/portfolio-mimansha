9a081076a9c36d94b48f9a80e77228adbd202130
mimansha23 <mimanshapandey89@gmail.com>
2026-06-20 23:21:47 +0530

    changing the designs

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ const skillObserver = new IntersectionObserver(entries=>{
 skillBars.forEach(bar=>{
     skillObserver.observe(bar);
 });
+const heroTitle =
+document.querySelector(".hero-title");
+
+let hue = 0;
+
+setInterval(()=>{
+
+    hue += 1;
+
+    heroTitle.style.background =
+    `linear-gradient(
+        90deg,
+        hsl(${hue},100%,60%),
+        hsl(${hue+100},100%,60%)
+    )`;
+
+    heroTitle.style.webkitBackgroundClip="text";
+    heroTitle.style.color="transparent";
+
+},30);
+window.addEventListener("load", () => {
+
+    const loader =
+    document.querySelector(".loader");
+
+    loader.classList.add("hide");
+
+    setTimeout(() => {
+        loader.remove();
+    }, 1000);
+
+});
